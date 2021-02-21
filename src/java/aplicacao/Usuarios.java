c618a09584cde201ac81e440c7042cde62aba469
Mathunes <matheusantunes720@gmail.com>
2021-02-21 00:52:16 -0300

    Adicao dos atributos e metodos da classe Usuarios

--- src/java/aplicacao/Usuarios.java
+++ src/java/aplicacao/Usuarios.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aplicacao;
 
-/**
- *
- * @author Felip
- */
 public class Usuarios {
+    
+    private int id;
+    private String nome;
+    private String cpf;
+    private String senha;
+    private char tipo;
+
+    public Usuarios(int id, String nome, String cpf, String senha, char tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+    
+    
     
 }
