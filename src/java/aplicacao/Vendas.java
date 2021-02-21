983aa02406cc3927f084b3f0f9b0b653eacf9fba
Mathunes <matheusantunes720@gmail.com>
2021-02-21 00:55:07 -0300

    Adicao dos atributos e metodos da classe Vendas

--- src/java/aplicacao/Vendas.java
+++ src/java/aplicacao/Vendas.java
@@ -1,14 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aplicacao;
 
-/**
- *
- * @author Felip
- */
 public class Vendas {
+
+    private int id;
+    private int quantidadeVenda;
+    private String dataVenda;
+    private double valorVenda;
+    private int idCliente;
+    private int idProduto;
+    private int idVendedor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidadeVenda() {
+        return quantidadeVenda;
+    }
+
+    public void setQuantidadeVenda(int quantidadeVenda) {
+        this.quantidadeVenda = quantidadeVenda;
+    }
+
+    public String getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(String dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public double getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public Vendas(int id, int quantidadeVenda, String dataVenda, double valorVenda, int idCliente, int idProduto, int idVendedor) {
+        this.id = id;
+        this.quantidadeVenda = quantidadeVenda;
+        this.dataVenda = dataVenda;
+        this.valorVenda = valorVenda;
+        this.idCliente = idCliente;
+        this.idProduto = idProduto;
+        this.idVendedor = idVendedor;
+    }
     
 }
