3f67efea41b912a1cfdee80e77512acc98cd1a71
Mathunes <matheusantunes720@gmail.com>
2021-02-21 01:00:51 -0300

    Adicao dos atributos e metodos da classe Categorias

--- src/java/aplicacao/Categorias.java
+++ src/java/aplicacao/Categorias.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aplicacao;
 
-/**
- *
- * @author Felip
- */
 public class Categorias {
+
+    private int id;
+    private String nomeCategoria;
+
+    public Categorias(int id, String nomeCategoria) {
+        this.id = id;
+        this.nomeCategoria = nomeCategoria;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
     
 }
