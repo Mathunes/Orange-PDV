cbae02116c1f8e1feefc6d89a350cef417632c84
Mathunes <matheusantunes720@gmail.com>
2021-02-24 13:23:21 -0300

    Adição do método para inserir/atualizar usuário no banco de dados

--- src/java/model/ClientesDAO.java
+++ src/java/model/ClientesDAO.java
@@ -7,8 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
@@ -88,5 +86,46 @@ public class ClientesDAO extends HttpServlet {
         }
         
         return cliente;
+    }
+    
+    public boolean gravar(Clientes cliente) {
+        
+        try {
+            String sql;
+            
+            if (cliente.getId() == 0) {
+                sql = "INSERT INTO clientes "
+                        + "(nome, cpf, endereco, bairro, cidade, uf, cep, telefone, email)"
+                        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            } else {
+                sql = "UPSATE clientes SET "
+                        + "nome=?, cpf=?, endereco=?, bairro=?, cidade=?, uf=?, cep=?, telefone=?, email=?"
+                        + "WHERE id=?";
+            }
+            
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            ps.setString(1, cliente.getNome());
+            ps.setString(2, cliente.getCpf());
+            ps.setString(3, cliente.getEndereco());
+            ps.setString(4, cliente.getBairro());
+            ps.setString(5, cliente.getCidade());
+            ps.setString(6, cliente.getUf());
+            ps.setString(7, cliente.getCep());
+            ps.setString(8, cliente.getTelefone());
+            ps.setString(9, cliente.getEmail());
+            
+            if (cliente.getId() > 0)
+                ps.setInt(10, cliente.getId());
+            
+            ps.execute();
+            
+            return true;
+            
+        } catch (SQLException ex) {
+            System.out.println("Erro de SQL: " + ex.getMessage());
+            
+            return false;
+        }
+            
     }
 }
