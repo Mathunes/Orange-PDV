8956fa2a5215e36842b111def67e1ab207e8b9b3
Mathunes <matheusantunes720@gmail.com>
2021-02-24 01:55:45 -0300

    Criação do Controller e DAO de clientes

--- src/java/aplicacao/Clientes.java
+++ src/java/aplicacao/Clientes.java
@@ -11,22 +11,7 @@ public class Clientes {
     private String uf;
     private String cep;
     private String telefone;
-    private String email;
-
-    public Clientes(int id, String nome, String cpf, String endereco, String bairro, String cidade, String uf, String cep, String telefone, String email) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.uf = uf;
-        this.cep = cep;
-        this.telefone = telefone;
-        this.email = email;
-    }
-
-    
+    private String email;   
     
     public int getId() {
         return id;
--- src/java/controller/ClientesController.java
+++ src/java/controller/ClientesController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import aplicacao.Clientes;
 import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.ClientesDAO;
 
 @WebServlet(name = "ClientesController", urlPatterns = {"/ClientesController"})
 public class ClientesController extends HttpServlet {
@@ -13,6 +17,24 @@ public class ClientesController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        //Impedir que a página seja armazena em cache, impedindo a função "voltar" do navegador
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        response.setHeader("Expires", "0"); // Proxies.
+        
+        ClientesDAO dao = new ClientesDAO();
+        String acao = (String) request.getParameter("acao");
+        ArrayList<Clientes> clientes;
+        
+        switch (acao) {
+            case "mostrar":
+                clientes = dao.getClientes();
+                request.setAttribute("clientes", clientes);
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/aeclientes.jsp");
+                rd.forward(request, response);
+                break;
+        }
         
     }
 
