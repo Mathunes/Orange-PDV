9302acf8da8bae3fd60810ba03bde1cb28054329
Mathunes <matheusantunes720@gmail.com>
2021-03-11 22:52:15 -0300

    Adição da validação do cpf no servidor

--- src/java/controller/ClientesController.java
+++ src/java/controller/ClientesController.java
@@ -145,6 +145,8 @@ public class ClientesController extends HttpServlet {
             mensagem = "Telefone deve conter no máximo 20 caracteres";
         else if (email.length() > 50)
             mensagem = "Email deve conter no máximo 50 caracteres";
+        else if (!validaCPF(cpf))
+            mensagem = "CPF inválido";
         else {
             
             cliente.setId(id);
@@ -171,6 +173,41 @@ public class ClientesController extends HttpServlet {
         RequestDispatcher rd = request.getRequestDispatcher("/clientes.jsp");
         rd.forward(request, response);
         
+    }
+    
+    public static boolean validaCPF(String cpf) {
+        double soma = 0;
+        double resto;
+        
+        cpf = cpf.replaceAll("\\.", "");
+        cpf = cpf.replaceAll("\\-", "");
+        
+        if ((cpf.equals("00000000000")) || (cpf.equals("11111111111")) || (cpf.equals("22222222222")) 
+            || (cpf.equals("33333333333")) || (cpf.equals("44444444444")) || (cpf.equals("55555555555")) 
+            || (cpf.equals("66666666666")) || (cpf.equals("77777777777")) || (cpf.equals("88888888888")) 
+            || (cpf.equals("99999999999")))
+            return false;
+        
+        for (int i=1; i<=9; i++)
+            soma += (Integer.parseInt(cpf.substring(i-1, i)) * (11 - i));
+        resto = (soma * 10) % 11;
+        
+        if ((resto == 10) || (resto == 11))
+            resto = 0;
+        if (resto != Integer.parseInt(cpf.substring(9,10))) 
+            return false;
+        
+        soma = 0;
+        for (int i = 1; i <= 10; i++)
+            soma += (Integer.parseInt(cpf.substring(i-1, i)) * (12 - i));
+        resto = (soma * 10) % 11;
+
+        if ((resto == 10) || (resto == 11))
+            resto = 0;
+        if (resto != Integer.parseInt(cpf.substring(10,11))) 
+            return false;
+        return true;
+
     }
 
 }
